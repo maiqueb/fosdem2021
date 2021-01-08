3aeb50bc2179a388e0280fc4be6c5a5f9e68bdfd
Miguel Duarte Barroso <mdbarroso@redhat.com>
2021-01-08 16:22:14 +0100

    Showcase how CAP_NET_RAW impacts binding a DGRAM socket to a device
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,16 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 
 	"golang.org/x/sys/unix"
+)
+
+const (
+	infiniteLease = 999 * 24 * time.Hour
 )
 
 func main() {
@@ -53,7 +58,48 @@ func main() {
 		},
 	}
 
-	rootCmd.AddCommand(createRawSocket)
+	bindToDevice := &cobra.Command{
+		Use:  "bind-to-device",
+		Args: cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ifaceName := args[0]
+			port, err := cmd.Flags().GetUint("port")
+			if err != nil {
+				return fmt.Errorf("could not parse the port number: %v", err)
+			}
+
+			glog.Infof("Will create a DGRAM socket on interface: %s", ifaceName)
+			fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_DGRAM, unix.IPPROTO_UDP)
+			if err != nil {
+				return fmt.Errorf("cannot get a DGRAM socket: %v", err)
+			}
+			f := os.NewFile(uintptr(fd), "")
+			// net.FilePacketConn dups the FD, so we have to close this in any case.
+			defer f.Close()
+
+			// Bind directly to the interface.
+			if err := unix.BindToDevice(fd, ifaceName); err != nil {
+				if errno, ok := err.(unix.Errno); ok && errno == unix.EPERM {
+					// Return a more helpful error message in this (fairly common) case
+					return fmt.Errorf("cannot bind to interface without CAP_NET_RAW or root permissions")
+				}
+				return fmt.Errorf("cannot bind to interface %s: %v", ifaceName, err)
+			}
+			glog.Infof("Succeeded in binding to device %s, port %d", ifaceName, port)
+
+			saddr := unix.SockaddrInet6{Port: int(port)}
+			copy(saddr.Addr[:], net.IPv6unspecified)
+			if err := unix.Bind(fd, &saddr); err != nil {
+				return fmt.Errorf("cannot bind to address %v: %v", saddr, err)
+			}
+
+			glog.Infof("Created a UDP socket bound to device: %s", ifaceName)
+			return nil
+		},
+	}
+
+	bindToDevice.Flags().Uint("port", 547, "specify the port to bind to")
+	rootCmd.AddCommand(createRawSocket, bindToDevice)
 
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
